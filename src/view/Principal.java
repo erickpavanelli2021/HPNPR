d369c64cbe7d6784081b11e320a02d0745d0b150
erick.pavanelli <erick.pavanelli@Analista-02.mendel.com>
2021-09-17 13:42:04 -0300

    Calculadora Hp NPR

---
+++ src/view/Principal.java
@@ -0,0 +1,72 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import br.edu.fateczl.erick.pilhaint.Pilha;
+import controller.CalculadoraHPNPR;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Pilha pilha = new Pilha();
+		CalculadoraHPNPR control = new CalculadoraHPNPR();
+
+		int opc = 0;
+		int res = 0;
+
+		String[] botao = { "- Incluir numero -", "- Operacao -", "- Sair -" };
+		
+		String[] operacao = { "Soma", "Subtracao", "Multiplicacao", "Divisao", "Sair" };
+		
+		
+		
+		do { // Executa caixa enquanto estiver inserindo numeros
+			
+			
+			opc = JOptionPane.showOptionDialog(null, "\n Escolha uma opcao \n\n", " >>> MENU <<< ", JOptionPane.INFORMATION_MESSAGE, JOptionPane.QUESTION_MESSAGE, null, botao, botao[0]);
+			
+			if (opc == 0) {
+				
+				int num = Integer.parseInt(JOptionPane.showInputDialog(null, "\n Digite um numero: \n\n"));
+				control.insereValor(pilha, num);
+			}
+			
+		} while ((opc != 1) && (opc != 2));
+
+		
+		if (opc == 1) { //Selecionou Botao Operacao
+			
+			 do {
+				 
+			 
+				int opcao = JOptionPane.showOptionDialog(null, "\n Escolha a Operacao \n\n ", ">> OPERACOES <<<", JOptionPane.INFORMATION_MESSAGE, JOptionPane.QUESTION_MESSAGE, null, operacao, operacao[2]);
+				
+				if (opcao == 4) { //Clicou em Sair
+					
+					System.out.println(">>> ATE A PROXIMA <<<");
+					System.exit(0);
+				}
+					
+				else {
+				
+					try {
+						
+					res = control.npr(pilha, opcao);
+					control.insereValor(pilha, res);
+					
+					
+					} catch (Exception e) {
+						JOptionPane.showMessageDialog(null,"Resultado: " + res);
+					e.printStackTrace();
+					System.exit(0);
+					}
+				}
+				
+} while (!pilha.isEmpty());
+			 
+			 
+		}
+
+	}
+
+}
